7beafe2ca1f7b0fa34e18ddc7745ae9922c33574
lucasx0 <lucas.almeidasilva49@gmail.com>
2022-05-02 10:20:59 -0300

    Adicionando diretivas a pag todo-list e criando interface tasklist.

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+/*     {task: "minha Nova Task", checked: true},
+    {task: "minha Nova Task 2", checked: false} */
+  ];
 
   constructor() { }
 
